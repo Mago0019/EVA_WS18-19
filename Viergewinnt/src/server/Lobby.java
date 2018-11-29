7488f2fe2a1de60461771448c3458ebc4f0ffa4e
mago0019 <zlog.manu@gmail.com>
2018-11-29 16:19:53 +0100

    test fxml

--- Viergewinnt/src/server/Lobby.java
+++ Viergewinnt/src/server/Lobby.java
@@ -3,6 +3,7 @@ package server;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 /**
  * Thread-Klasse die alle aktiven Clients des Servers verwaltet.
@@ -11,11 +12,14 @@ public class Lobby extends Thread {
 
 	List<Player> lobbyList;
 	List<Player> inGameList;
-	// TODO
+	ExecutorService tPool;
+	private int gameCount;
 
-	public Lobby() {	
+	public Lobby(ExecutorService threadPool) {	
 		this.lobbyList = new ArrayList<Player>();
 		this.inGameList = new ArrayList<Player>();
+		this.tPool = threadPool;
+		this.gameCount = 0;
 	}
 
 	@Override
@@ -34,12 +38,15 @@ public class Lobby extends Thread {
 		this.lobbyList.add(pl);
 	}
 	
-	private void startGameSession(Player p1, Player p2) {
+	private void startGameSession(Player player1, Player player2) {
+		gameCount++;
 		try {
+			String gameName = "Game " + gameCount;  // TODO: bessere Namensgenerierung
+			GameSession gs = new GameSession(player1, player2, gameName); 
 			
 			
-			inGameList.add(p1);
-			inGameList.add(p2);
+			inGameList.add(player1);
+			inGameList.add(player2);
 			
 		} catch (Exception e) {
 			System.out.println("ERROR: StartGameSession fehlgeschlagen!");
