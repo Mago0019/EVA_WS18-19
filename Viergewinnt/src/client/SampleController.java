f7bca0e2f2e06e7f48f624d41fccf0d4d7057b0d
Asriel <Asriel108@Web.de>
2018-11-30 08:06:27 +0100

    Nur unnötige Imports gelöscht

--- Viergewinnt/src/client/SampleController.java
+++ Viergewinnt/src/client/SampleController.java
@@ -9,10 +9,8 @@ package client;
  */
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
