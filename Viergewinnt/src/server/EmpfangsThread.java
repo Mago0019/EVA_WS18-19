686f95f7b50cf5f5ac84d456fc28234da5b42533
Asriel <Asriel108@Web.de>
2018-12-03 18:28:52 +0100

    Aufgaben in Methoden ausgelagert und ToDos eingebaut

--- Viergewinnt/src/server/EmpfangsThread.java
+++ Viergewinnt/src/server/EmpfangsThread.java
@@ -1,57 +1,110 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
 /**
- * Kleine Thread-Klasse, die den Clienten nur nach dem Namen fragt und ihn dann
- * in die Lobby einträgt.
+ * Kleine Thread-Klasse, die den Clienten nach dem Namen fragt und ihn dann in
+ * die Lobby einträgt. Danach wird die Kommunikation mit der Lobby geregelt.
+ * 
+ * IDEE: Startet die GameSessions aus der Lobby. Kümmert sich also weiter um den
+ * Clienten.
  */
 public class EmpfangsThread extends Thread {
 
 	Socket clientSocket;
+	String clientName;
 	Lobby lobby;
+	boolean running;
+	BufferedReader input;
+	PrintStream output;
 
 	public EmpfangsThread(Socket clientSocket, Lobby lobby) {
 		this.clientSocket = clientSocket;
+		this.clientName = "noName";
 		this.lobby = lobby;
+		this.running = true;
 	}
 
 	@Override
 	public void run() {
 		System.out.println("EmpfangsThread gestartet.");
-		// Frage den Clienten nach dem Namen und geben ihn dann an die Lobby weiter.
-		String clientName = "noName";
-		boolean done = false;
 
-		while (!done) {
+		// Besorge Namen für Spieler
+		askForPlayerName();
+
+		// In die Lobby einfügen
+		lobby.addUser(clientSocket, clientName);
+
+		// jetzt die Kommunikation regeln
+		listenToClient();
+	}
+
+	private void listenToClient() {
+		// TODO: darauf warten, dass der Client eine Aktion in der Lobby ausführen will
+		String msg = "";
+		while (running) {
 			try {
-				BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				PrintStream output = new PrintStream(clientSocket.getOutputStream());
+				
+				// TODO: Marshalling  - erste 4 Bytes (Befehlscode) anschauen.
+				msg = input.readLine();
+				
+				switch (msg) {
+				case "~~2": // z.B. Starte SpielLobby
+					break;
+
+				case "~~3": // z.B. joine anderer Lobby
+					break;
+					
+				default:
+					break;
+				}
+				
+				
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+
+	}
+
+	/**
+	 * Client wird angefragt mit Befehlscode (TODO) "~~0" einen SpielerNamen
+	 * auszusuchen.
+	 */
+	private void askForPlayerName() {
+		boolean done = false;
+		String newName = null;
+		int tryCount = 0;
+
+		while (!done && tryCount < 5) {
+			try {
+				this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				this.output = new PrintStream(clientSocket.getOutputStream());
 
 				// TODO: Befehlscode für NamensAnforderung
 				output.print("~~0 \n");
-				//output.flush();
+				// output.flush();
 				System.out.println("Client angefragt - Code: ~~0");
 
 				// TODO: Namen empfangen / Marshalling + DeMarshalling in extra MEthoden
-				String newName = input.readLine();
+				newName = input.readLine();
 				System.out.println("Antwort von Client: " + newName);
-				
+
 				// neuen Namen überprüfen
-				if(newName != null && newName.length() > 2 && newName.length() < 10) {
-					clientName = newName;
+				if (newName != null && newName.length() > 2 && newName.length() < 10) {
+					this.clientName = newName;
 					done = true;
 				}
+				tryCount++;
 
 			} catch (Exception e) {
+				tryCount++;
 				System.out.println("ERROR: Abfragen des ClientNamens fehlgeschlagen!");
 			}
-		}// end While
-		
-		//In die Lobby einfügen
-		lobby.addUser(clientSocket, clientName);
+		} // end While
 	}
 }
